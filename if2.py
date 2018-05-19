e371efb04bb86458a718a3d41e1672378133254c
Daniel Dzhanlatian <dzhanlatyan@gmail.com>
2018-05-19 15:19:10 +0300

    Проба пера

---
+++ if2.py
@@ -0,0 +1,14 @@
+
+input1 = input("введите слово ")
+input2 = input("введите слово 2 ")
+
+if len(input1) == len(input2):
+	print(input1)
+elif len(input1) >= len(input2):
+	print(input2)
+elif len(input1) > len(input2):
+	if input2 = "learn":
+	print(learn)
+
+else:
+	print(ошибка)
